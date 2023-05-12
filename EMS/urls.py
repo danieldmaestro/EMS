80172d131bc07ac91a1c512bc00fcac549c2f109
Daniel Momodu <momodufresh@gmail.com>
2023-05-12 16:44:01 +0100

    urls.py edit

--- EMS/urls.py
+++ EMS/urls.py
@@ -36,7 +36,10 @@ urlpatterns = [
          include("staff.api.urls", namespace="staff_api")),
     path('ems/<slug:org_slug>/dashboard/admin/', include('organization.urls')),
     path('ems/<slug:org_slug>/dashboard/staff/', include('staff.urls')),
+]
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
